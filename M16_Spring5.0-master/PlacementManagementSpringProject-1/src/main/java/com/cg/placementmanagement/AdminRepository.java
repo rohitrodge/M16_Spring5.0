7b47a870a43146d71d86023a3118bcf607aadcbc
rohit <rohit@LAPTOP-LCB6VDM3>
2022-06-27 15:37:27 +0530

    first commit

--- M16_Spring5.0-master/PlacementManagementSpringProject-1/src/main/java/com/cg/placementmanagement/AdminRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cg.placementmanagement;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<Admin,Integer> {
-
-}
--- M16_Spring5.0-master/PlacementManagementSpringProject-1/src/main/java/com/cg/placementmanagement/PlacementRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cg.placementmanagement;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlacementRepository  extends JpaRepository<Placement,Integer> {
-
-}
